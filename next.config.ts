de4420a640c90d634e16157c16b3998c865aecfa
Varsha Shekhawat <varsha.23bce7221@vitapstudent.ac.in>
2026-03-02 16:32:06 +0530

    Removed invalid next configs and old middleware

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
